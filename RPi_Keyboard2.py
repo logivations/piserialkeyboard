a0081dea5953784cdfc59dc2b8e537518ab2da9c
ahsan <ahsan.shahensha@logivations.com>
2019-05-06 16:21:57 +0200

    updated keyboard

--- RPi_Keyboard2.py
+++ RPi_Keyboard2.py
@@ -5,23 +5,32 @@ kb = KeyboardLayoutUS()
 
 NULL_CHAR = chr(0)
 import time
-def write_report(report):
-    with open('/dev/hidg0', 'rb+') as fd:
-        fd.write(report.encode())
-time.sleep(2)
+class Keyboard():
+    def __init__(self):
+        pass
+
+    def __write_report(report):
+        with open('/dev/hidg0', 'rb+') as fd:
+            fd.write(report.encode())
+    time.sleep(2)
+    def keyout(self,k):
+        for i in k:
+            key = kb.keycodes(i)
+            self.__write_report(chr(key[0])+NULL_CHAR+chr(key[1])+NULL_CHAR*5)
+            self.__write_report(NULL_CHAR*8)
+        self.__write_report(NULL_CHAR*2+chr(kb.keycodes('\n')[1])+NULL_CHAR*5)
+        self.__write_report(NULL_CHAR*8)
+
 # Press a
-write_report(NULL_CHAR*2+chr(4)+NULL_CHAR*5)
+#write_report(NULL_CHAR*2+chr(4)+NULL_CHAR*5)
 # Release keys
-write_report(NULL_CHAR*8)
+#write_report(NULL_CHAR*8)
 # Press SHIFT + a = A
-write_report(chr(32)+NULL_CHAR+chr(4)+NULL_CHAR*5)
+#write_report(chr(32)+NULL_CHAR+chr(4)+NULL_CHAR*5)
 
-write_report(NULL_CHAR*8)
+#write_report(NULL_CHAR*8)
 
-for i in 'Hello World':
-    key=kb.keycodes(i)
-    write_report(chr(key[0])+NULL_CHAR+chr(key[1])+NULL_CHAR*5)
-    write_report(NULL_CHAR*8)
-write_report(NULL_CHAR*2+chr(kb.keycodes('\n')[1])+NULL_CHAR*5)
-write_report(NULL_CHAR*8)
+if __name__ == '__main__':
+    kb = Keyboard()
+    kb.keyout('Hello World 111')
 
